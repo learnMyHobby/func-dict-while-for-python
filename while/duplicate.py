c93ab56d0e5e6f525b4c6e83a272e34c4b81fcc1
Bibek <ilovecoding643@gmail.com>
2021-07-19 16:56:36 +0545

    [adds] removes the duplicate value from the list using while

---
+++ while/duplicate.py
@@ -0,0 +1,11 @@
+# bird = ['crows','pigeon','eagles','falcon','pigeon','falcon','falcon']
+# Remove all the duplicates from the following list using while
+
+import math
+
+bird = ['crows','pigeon','eagles','falcon','pigeon','falcon','falcon']
+
+while True:
+    removeDuplicate = set(bird)
+    print(removeDuplicate)
+    break
